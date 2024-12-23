282396d9f5b0e51b34c20ce220284755d7736589
Anudeep Yesupogu <anudeep.yesupogu@valuelabs.com>
2024-12-23 23:44:39 +0530

    modified

--- remote_state_backend.tf
+++ remote_state_backend.tf
@@ -1,38 +1,55 @@
 resource "aws_s3_bucket" "terraform_state" {
   bucket = "terraform-state-bucket-inhouse-website-api"
-  acl    = "private"
- 
+
   versioning {
     enabled = true
   }
- 
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
+
+  lifecycle_rule {
+    enabled = true
+    expiration {
+      days = 30
     }
   }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state" {
+  bucket = aws_s3_bucket.terraform_state.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "terraform_state" {
+  bucket = aws_s3_bucket.terraform_state.id
+
+  block_public_acls   = true
+  block_public_policy = true
+  ignore_public_acls  = true
+  restrict_public_buckets = true
 }
 
 resource "aws_dynamodb_table" "terraform_locks" {
   name         = "terraform-state-locks-inhouse-website-api"
   billing_mode = "PAY_PER_REQUEST"
- 
+
   attribute {
     name = "LockID"
     type = "S"
   }
- 
+
   hash_key = "LockID"
 }
 
 terraform {
   backend "s3" {
-	bucket     	= "terraform-state-bucket-inhouse-website-api"
-	key        	= "state/statefile.tfstate"
-	region     	= "${var.aws_region}"
-	encrypt    	= true
-	dynamodb_table = "terraform-state-locks-inhouse-website-api"
+    bucket      = "terraform-state-bucket-inhouse-website-api"
+    key         = "state/statefile.tfstate"
+    region      = "${var.aws_region}"
+    encrypt     = true
+    dynamodb_table = "terraform-state-locks-inhouse-website-api"
   }
 }
