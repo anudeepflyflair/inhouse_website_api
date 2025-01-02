1c9c204e924711ad38459fc68194daa2950c1221
Anudeep Yesupogu <anudeep.yesupogu@valuelabs.com>
2025-01-02 20:44:01 +0530

    modified

--- s3.tf
+++ s3.tf
@@ -1,27 +1,5 @@
-# # Define the S3 bucket to store Lambda layer files
-# resource "aws_s3_bucket" "lambda_layer_bucket" {
-#   bucket = "lambda-layer-files-bucket"  
-# #   acl    = "private"                    
- 
-#   tags = {
-#     Name        = "Lambda Layer Bucket"
-#   }
-# }
- 
-# # Define the S3 object for the Lambda layer file
-# resource "aws_s3_bucket_object" "common_dependencies" {
-#   bucket = aws_s3_bucket.lambda_layer_bucket.id
-#   key    = "lambda_layers/common_dependencies.zip"  
-#   source = "./lambda_layer_files/common_dependencies.zip"  
-# #   acl    = "private"  
- 
-#   tags = {
-#     Name        = "Common Dependencies Layer"
-#   }
-# }
-
 resource "aws_s3_bucket" "lambda_layer_bucket" {
-  bucket              = "terraform-state-bucket-inhouse-website-api"
+  bucket              = "lamdazipfilesforwenbsitedevaccount"
 #   acl                 = "private"
   force_destroy       = true
   object_lock_enabled = false
