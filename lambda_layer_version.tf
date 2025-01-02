141382633a7ee099adb428ee159842999cd2e7e9
Anudeep Yesupogu <anudeep.yesupogu@valuelabs.com>
2025-01-02 20:48:19 +0530

    modified

--- lambda_layer_version.tf
+++ lambda_layer_version.tf
@@ -3,8 +3,6 @@ resource "aws_lambda_layer_version" "lambda_layer" {
   layer_name          = "python_dependencies_layer"
   compatible_runtimes = ["python3.9", "python3.10", "python3.11", "python3.12"]  
   s3_bucket           = aws_s3_bucket.lambda_layer_bucket.id
-  s3_key              = aws_s3_bucket_object.common_dependencies.key  
+  s3_key              = aws_s3_bucket.common_dependencies.key  
   description         = "Layer for Python dependencies compatible with Python 3.9, 3.10, 3.11, and 3.12 runtimes"
 }
-
-
