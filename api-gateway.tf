cdb2a7192867f3891d1587bdcc48d15558521ac9
Anudeep Yesupogu <anudeep.yesupogu@valuelabs.com>
2025-01-03 20:02:35 +0530

    added jenkins pipeline

---
+++ api-gateway.tf
@@ -0,0 +1,214 @@
+# Create an API Gateway REST API
+resource "aws_api_gateway_rest_api" "website_dev" {
+  name        = "Website-Dev"
+  description = "API Gateway for Website Dev"
+}
+
+# Create API Gateway resources
+resource "aws_api_gateway_resource" "accessibility_request" {
+  rest_api_id = aws_api_gateway_rest_api.website_dev.id
+  parent_id   = aws_api_gateway_rest_api.website_dev.root_resource_id
+  path_part   = "accessibilityRequest"
+}
+
+resource "aws_api_gateway_resource" "ancillary_search" {
+  rest_api_id = aws_api_gateway_rest_api.website_dev.id
+  parent_id   = aws_api_gateway_rest_api.website_dev.root_resource_id
+  path_part   = "searchAncillary"
+}
+
+resource "aws_api_gateway_resource" "create_reservation" {
+  rest_api_id = aws_api_gateway_rest_api.website_dev.id
+  parent_id   = aws_api_gateway_rest_api.website_dev.root_resource_id
+  path_part   = "createReservation"
+}
+
+resource "aws_api_gateway_resource" "flight_selection" {
+  rest_api_id = aws_api_gateway_rest_api.website_dev.id
+  parent_id   = aws_api_gateway_rest_api.website_dev.root_resource_id
+  path_part   = "flightSelect"
+}
+
+resource "aws_api_gateway_resource" "low_fares" {
+  rest_api_id = aws_api_gateway_rest_api.website_dev.id
+  parent_id   = aws_api_gateway_rest_api.website_dev.root_resource_id
+  path_part   = "lowFares"
+}
+
+resource "aws_api_gateway_resource" "payment_methods" {
+  rest_api_id = aws_api_gateway_rest_api.website_dev.id
+  parent_id   = aws_api_gateway_rest_api.website_dev.root_resource_id
+  path_part   = "paymentMethods"
+}
+
+resource "aws_api_gateway_resource" "payment_reservation" {
+  rest_api_id = aws_api_gateway_rest_api.website_dev.id
+  parent_id   = aws_api_gateway_rest_api.website_dev.root_resource_id
+  path_part   = "paymentReservation"
+}
+
+resource "aws_api_gateway_resource" "seat_selection" {
+  rest_api_id = aws_api_gateway_rest_api.website_dev.id
+  parent_id   = aws_api_gateway_rest_api.website_dev.root_resource_id
+  path_part   = "seatSelection"
+}
+
+resource "aws_api_gateway_resource" "voucher_code" {
+  rest_api_id = aws_api_gateway_rest_api.website_dev.id
+  parent_id   = aws_api_gateway_rest_api.website_dev.root_resource_id
+  path_part   = "voucherCode"
+}
+
+# Create API Gateway methods
+resource "aws_api_gateway_method" "accessibility_request_get" {
+  rest_api_id = aws_api_gateway_rest_api.website_dev.id
+  resource_id = aws_api_gateway_resource.accessibility_request.id
+  http_method = "GET"
+  authorization = "CUSTOM"
+}
+
+resource "aws_api_gateway_method" "ancillary_search_get" {
+  rest_api_id = aws_api_gateway_rest_api.website_dev.id
+  resource_id = aws_api_gateway_resource.ancillary_search.id
+  http_method = "GET"
+  authorization = "CUSTOM"
+}
+
+resource "aws_api_gateway_method" "create_reservation_post" {
+  rest_api_id = aws_api_gateway_rest_api.website_dev.id
+  resource_id = aws_api_gateway_resource.create_reservation.id
+  http_method = "POST"
+  authorization = "CUSTOM"
+}
+
+resource "aws_api_gateway_method" "flight_selection_get" {
+  rest_api_id = aws_api_gateway_rest_api.website_dev.id
+  resource_id = aws_api_gateway_resource.flight_selection.id
+  http_method = "GET"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_method" "low_fares_get" {
+  rest_api_id = aws_api_gateway_rest_api.website_dev.id
+  resource_id = aws_api_gateway_resource.low_fares.id
+  http_method = "GET"
+  authorization = "CUSTOM"
+}
+
+resource "aws_api_gateway_method" "payment_methods_get" {
+  rest_api_id = aws_api_gateway_rest_api.website_dev.id
+  resource_id = aws_api_gateway_resource.payment_methods.id
+  http_method = "GET"
+  authorization = "CUSTOM"
+}
+
+resource "aws_api_gateway_method" "payment_reservation_post" {
+  rest_api_id = aws_api_gateway_rest_api.website_dev.id
+  resource_id = aws_api_gateway_resource.payment_reservation.id
+  http_method = "POST"
+  authorization = "CUSTOM"
+}
+
+resource "aws_api_gateway_method" "seat_selection_get" {
+  rest_api_id = aws_api_gateway_rest_api.website_dev.id
+  resource_id = aws_api_gateway_resource.seat_selection.id
+  http_method = "GET"
+  authorization = "CUSTOM"
+}
+
+resource "aws_api_gateway_method" "voucher_code_get" {
+  rest_api_id = aws_api_gateway_rest_api.website_dev.id
+  resource_id = aws_api_gateway_resource.voucher_code.id
+  http_method = "GET"
+  authorization = "CUSTOM"
+}
+
+# Create API Gateway integrations
+resource "aws_api_gateway_integration" "accessibility_request_get" {
+  rest_api_id = aws_api_gateway_rest_api.website_dev.id
+  resource_id = aws_api_gateway_resource.accessibility_request.id
+  http_method = aws_api_gateway_method.accessibility_request_get.http_method
+  integration_http_method = "GET"
+  type        = "LAMBDA"
+  uri         = "arn:aws:apigateway:ca-central-1:lambda:path/2015-03-31/functions/arn:aws:lambda:ca-central-1:888577036740:function:accessibilityRequest/invocations"
+}
+
+resource "aws_api_gateway_integration" "ancillary_search_get" {
+  rest_api_id = aws_api_gateway_rest_api.website_dev.id
+  resource_id = aws_api_gateway_resource.ancillary_search.id
+  http_method = aws_api_gateway_method.ancillary_search_get.http_method
+  integration_http_method = "GET"
+  type        = "LAMBDA"
+  uri         = "arn:aws:apigateway:ca-central-1:lambda:path/2015-03-31/functions/arn:aws:lambda:ca-central-1:888577036740:function:ancillarySearch/invocations"
+}
+
+resource "aws_api_gateway_integration" "create_reservation_post" {
+  rest_api_id = aws_api_gateway_rest_api.website_dev.id
+  resource_id = aws_api_gateway_resource.create_reservation.id
+  http_method = aws_api_gateway_method.create_reservation_post.http_method
+  integration_http_method = "POST"
+  type        = "LAMBDA"
+  uri         = "arn:aws:apigateway:ca-central-1:lambda:path/2015-03-31/functions/arn:aws:lambda:ca-central-1:888577036740:function:createReservation/invocations"
+}
+
+resource "aws_api_gateway_integration" "flight_selection_get" {
+  rest_api_id = aws_api_gateway_rest_api.website_dev.id
+  resource_id = aws_api_gateway_resource.flight_selection.id
+  http_method = aws_api_gateway_method.flight_selection_get.http_method
+  integration_http_method = "GET"
+  type        = "LAMBDA"
+  uri         = "arn:aws:apigateway:ca-central-1:lambda:path/2015-03-31/functions/arn:aws:lambda:ca-central-1:888577036740:function:flightSelection/invocations"
+}
+
+resource "aws_api_gateway_integration" "low_fares_get" {
+  rest_api_id = aws_api_gateway_rest_api.website_dev.id
+  resource_id = aws_api_gateway_resource.low_fares.id
+  http_method = aws_api_gateway_method.low_fares_get.http_method
+  integration_http_method = "GET"
+  type        = "LAMBDA"
+  uri         = "arn:aws:apigateway:ca-central-1:lambda:path/2015-03-31/functions/arn:aws:lambda:ca-central-1:888577036740:function:lowFares/invocations"
+}
+
+resource "aws_api_gateway_integration" "payment_methods_get" {
+  rest_api_id = aws_api_gateway_rest_api.website_dev.id
+  resource_id = aws_api_gateway_resource.payment_methods.id
+  http_method = aws_api_gateway_method.payment_methods_get.http_method
+  integration_http_method = "GET"
+  type        = "LAMBDA"
+  uri         = "arn:aws:apigateway:ca-central-1:lambda:path/2015-03-31/functions/arn:aws:lambda:ca-central-1:888577036740:function:paymentMethods/invocations"
+}
+
+resource "aws_api_gateway_integration" "payment_reservation_post" {
+  rest_api_id = aws_api_gateway_rest_api.website_dev.id
+  resource_id = aws_api_gateway_resource.payment_reservation.id
+  http_method = aws_api_gateway_method.payment_reservation_post.http_method
+  integration_http_method = "POST"
+  type        = "LAMBDA"
+  uri         = "arn:aws:apigateway:ca-central-1:lambda:path/2015-03-31/functions/arn:aws:lambda:ca-central-1:888577036740:function:paymentReservation/invocations"
+}
+
+resource "aws_api_gateway_integration" "seat_selection_get" {
+  rest_api_id = aws_api_gateway_rest_api.website_dev.id
+  resource_id = aws_api_gateway_resource.seat_selection.id
+  http_method = aws_api_gateway_method.seat_selection_get.http_method
+  integration_http_method = "GET"
+  type        = "LAMBDA"
+  uri         = "arn:aws:apigateway:ca-central-1:lambda:path/2015-03-31/functions/arn:aws:lambda:ca-central-1:888577036740:function:seatSelection/invocations"
+}
+
+resource "aws_api_gateway_integration" "voucher_code_get" {
+  rest_api_id = aws_api_gateway_rest_api.website_dev.id
+  resource_id = aws_api_gateway_resource.voucher_code.id
+  http_method = aws_api_gateway_method.voucher_code_get.http_method
+  integration_http_method = "GET"
+  type        = "LAMBDA"
+  uri         = "arn:aws:apigateway:ca-central-1:lambda:path/2015-03-31/functions/arn:aws:lambda:ca-central-1:888577036740:function:voucherCode/invocations"
+}
+
+# # Deploy the API Gateway
+# resource "aws_api_gateway_deployment" "website_dev" {
+#   depends_on = [aws_api_gateway_integration.accessibility_request_get]
+#   rest_api_id = aws_api_gateway_rest_api.website_dev.id
+#   stage_name  = "dev"
+# }
+
