9518b99bb916f85cc850f495aab6312886da3efb
Anudeep Yesupogu <anudeep.yesupogu@valuelabs.com>
2025-01-03 20:24:28 +0530

    added jenkins pipeline

--- api-gateway.tf
+++ api-gateway.tf
@@ -123,14 +123,14 @@ resource "aws_api_gateway_method" "voucher_code_get" {
   authorization = "CUSTOM"
 }
 
-# Create API Gateway integrations
+# Create API Gateway integration
 resource "aws_api_gateway_integration" "accessibility_request_get" {
   rest_api_id = aws_api_gateway_rest_api.website_dev.id
   resource_id = aws_api_gateway_resource.accessibility_request.id
   http_method = aws_api_gateway_method.accessibility_request_get.http_method
   integration_http_method = "GET"
-  type        = "AWS_PROXY"
-  uri         = aws_AWS_PROXY_function.accessibilityRequest_Website_Dev.arn
+  type        = "HTTP"
+  uri         = "https://${aws_api_gateway_rest_api.website_dev.execution_arn}/dev/v1/accessibilityRequest"
 }
 
 resource "aws_api_gateway_integration" "ancillary_search_get" {
@@ -138,8 +138,8 @@ resource "aws_api_gateway_integration" "ancillary_search_get" {
   resource_id = aws_api_gateway_resource.ancillary_search.id
   http_method = aws_api_gateway_method.ancillary_search_get.http_method
   integration_http_method = "GET"
-  type        = "AWS_PROXY"
-  uri         = aws_AWS_PROXY_function.ancillarySearch.arn
+  type        = "HTTP"
+  uri         = "https://${aws_api_gateway_rest_api.website_dev.execution_arn}/dev/v1/searchAncillary"
 }
 
 resource "aws_api_gateway_integration" "create_reservation_post" {
@@ -147,8 +147,8 @@ resource "aws_api_gateway_integration" "create_reservation_post" {
   resource_id = aws_api_gateway_resource.create_reservation.id
   http_method = aws_api_gateway_method.create_reservation_post.http_method
   integration_http_method = "POST"
-  type        = "AWS_PROXY"
-  uri         = aws_AWS_PROXY_function.createReservation_Web_Dev.arn
+  type        = "HTTP"
+  uri         = "https://${aws_api_gateway_rest_api.website_dev.execution_arn}/dev/v1/createReservation"
 }
 
 resource "aws_api_gateway_integration" "flight_selection_get" {
@@ -156,8 +156,8 @@ resource "aws_api_gateway_integration" "flight_selection_get" {
   resource_id = aws_api_gateway_resource.flight_selection.id
   http_method = aws_api_gateway_method.flight_selection_get.http_method
   integration_http_method = "GET"
-  type        = "AWS_PROXY"
-  uri         = aws_api_gateway_method.flight_selection_get.arn
+  type        = "HTTP"
+  uri         = "https://${aws_api_gateway_rest_api.website_dev.execution_arn}/dev/v1/flightSelect"
 }
 
 resource "aws_api_gateway_integration" "low_fares_get" {
@@ -165,8 +165,8 @@ resource "aws_api_gateway_integration" "low_fares_get" {
   resource_id = aws_api_gateway_resource.low_fares.id
   http_method = aws_api_gateway_method.low_fares_get.http_method
   integration_http_method = "GET"
-  type        = "AWS_PROXY"
-  uri         = aws_AWS_PROXY_function.accessibilityRequest_Website_Dev.arn
+  type        = "HTTP"
+  uri         = "https://${aws_api_gateway_rest_api.website_dev.execution_arn}/dev/v1/lowFares"
 }
 
 resource "aws_api_gateway_integration" "payment_methods_get" {
@@ -174,8 +174,8 @@ resource "aws_api_gateway_integration" "payment_methods_get" {
   resource_id = aws_api_gateway_resource.payment_methods.id
   http_method = aws_api_gateway_method.payment_methods_get.http_method
   integration_http_method = "GET"
-  type        = "AWS_PROXY"
-  uri         = aws_AWS_PROXY_function
+  type        = "HTTP"
+  uri         = "https://${aws_api_gateway_rest_api.website_dev.execution_arn}/dev/v1/paymentMethods"
 }
 
 resource "aws_api_gateway_integration" "payment_reservation_post" {
@@ -183,8 +183,8 @@ resource "aws_api_gateway_integration" "payment_reservation_post" {
   resource_id = aws_api_gateway_resource.payment_reservation.id
   http_method = aws_api_gateway_method.payment_reservation_post.http_method
   integration_http_method = "POST"
-  type        = "AWS_PROXY"
-  uri         = aws_AWS_PROXY_function.reservationPaymentTransaction_dev_IHW.arn
+  type        = "HTTP"
+  uri         = "https://${aws_api_gateway_rest_api.website_dev.execution_arn}/dev/v1/paymentReservation"
 }
 
 resource "aws_api_gateway_integration" "seat_selection_get" {
@@ -192,8 +192,8 @@ resource "aws_api_gateway_integration" "seat_selection_get" {
   resource_id = aws_api_gateway_resource.seat_selection.id
   http_method = aws_api_gateway_method.seat_selection_get.http_method
   integration_http_method = "GET"
-  type        = "AWS_PROXY"
-  uri         = aws_AWS_PROXY_function.seatSealection.arn
+  type        = "HTTP"
+  uri         = "https://${aws_api_gateway_rest_api.website_dev.execution_arn}/dev/v1/seatSelection"
 }
 
 resource "aws_api_gateway_integration" "voucher_code_get" {
@@ -201,14 +201,13 @@ resource "aws_api_gateway_integration" "voucher_code_get" {
   resource_id = aws_api_gateway_resource.voucher_code.id
   http_method = aws_api_gateway_method.voucher_code_get.http_method
   integration_http_method = "GET"
-  type        = "AWS_PROXY"
-  uri         = aws_AWS_PROXY_function.voucherCode_Web_Dev
+  type        = "HTTP"
+  uri         = "https://${aws_api_gateway_rest_api.website_dev.execution_arn}/dev/v1/voucherCode"
 }
 
-# # Deploy the API Gateway
+# # Deploy the API
 # resource "aws_api_gateway_deployment" "website_dev" {
 #   depends_on = [aws_api_gateway_integration.accessibility_request_get]
 #   rest_api_id = aws_api_gateway_rest_api.website_dev.id
 #   stage_name  = "dev"
 # }
-
